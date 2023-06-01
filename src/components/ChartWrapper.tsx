e2371abab6022b7867db82fe6b779b43edc60334
hermes7144 <kichancho@gmail.com>
2023-06-01 17:36:28 +0900

    feat:chart resize

--- src/components/ChartWrapper.tsx
+++ src/components/ChartWrapper.tsx
@@ -4,7 +4,7 @@ export default function ChartWrapper({ pct }) {
   return (
     <div className='flex flex-col items-center w-4/12'>
       <span className='text-lg py-3 text-gray-400'>{pct.pct}%</span>
-      <div className='max-w-[100px]'>
+      <div className='max-w-[90px]'>
         <DoughnutChart pct={pct} />
       </div>
       <span className='text-gray-400'>{pct.title}</span>
