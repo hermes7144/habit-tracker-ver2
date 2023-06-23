850063fc9bc2224dedb57c93de94142140dc8e03
hermes7144 <kichancho@gmail.com>
2023-06-23 14:08:37 +0900

    frequency

--- src/components/FrequencyChip.tsx
+++ src/components/FrequencyChip.tsx
@@ -1,7 +1,7 @@
 const WEEK = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
 
 export default function FrequencyChip({ frequency }) {
-  const className = 'flex items-center justify-center text-sm md:text-lg w-5 h-5 md:(w-10 h-10) rounded-full font-bold ';
+  const className = 'flex items-center justify-center text-sm md:text-lg w-10 h-10 rounded-full font-bold ';
   return (
     <div className='flex justify-center gap-[2px]'>
       {frequency &&
